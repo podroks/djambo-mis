9cac5eb3e40f8908419b2ae3dec51a21b4f537d3
podroks <bouilleteauflorian@outlook.fr>
2025-01-18 15:36:33 +0100

    Ajout du lazer

--- vue-project/src/components/threeJs/Player.vue
+++ vue-project/src/components/threeJs/Player.vue
@@ -51,8 +51,15 @@ defineExpose({ mesh, onHitPlayer, onStopHitPlayer });
 </script>
 
 <template>
-  <TresMesh ref="mesh" :position="[position.x, position.y, 0]">
-    <TresBoxGeometry :args="size" />
-    <TresMeshStandardMaterial :color="isHit ? '#225034' : '#77CA84'" />
-  </TresMesh>
+  <TresGroup :position="[position.x, position.y, 0]">
+    <TresMesh ref="mesh">
+      <TresBoxGeometry :args="size" />
+      <TresMeshStandardMaterial :color="isHit ? '#225034' : '#77CA84'" />
+    </TresMesh>
+
+    <TresMesh :position="[0, 0, -10]" :rotation="[Math.PI / 2, 0, 0]">
+      <TresCylinderGeometry :args="[0.015, 0.015, 20, 8]" />
+      <TresMeshStandardMaterial :color="'#4D9C64'" />
+    </TresMesh>
+  </TresGroup>
 </template>
